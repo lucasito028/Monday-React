dd782c6ac85c92d75b0dd1c0d2666a4adf45dd8c
lucasito028 <lucastoshihiroito28@gmail.com>
2024-09-11 12:34:54 -0300

    Melhorias

---
+++ src/api/tes.js
@@ -0,0 +1,29 @@
+import mondaySdk from "monday-sdk-js";
+
+monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQwOTI2MTMzMSwiYWFpIjoxMSwidWlkIjo2NTk5NzAxMCwiaWFkIjoiMjAyNC0wOS0xMVQxMjo1NTozMS42OTlaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU0MDU1NjYsInJnbiI6InVzZTEifQ.3BSo6yS7FCGzD2BcVYbs6gdfUCZtdfJquaSDUydpYgc')
+
+monday.api(`
+            query {
+        me {
+            name
+        }
+
+        # boards(ids: [13542, 68097]) {
+        boards(limit: 1) {
+            name
+
+            columns {
+            title
+            id
+            type
+            }
+
+            groups {
+                title
+            id
+            }
+
+            # Read more about Board type: https://developer.monday.com/api-reference/docs/boards
+        }
+        }
+  `);
