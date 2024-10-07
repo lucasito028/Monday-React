3ed3c0f9299cc99191dbf4aeff83cba68632304c
lucasito028 <lucastoshihiroito28@gmail.com>
2024-10-07 10:45:58 -0300

    MElhoreas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,60 @@
 import { useState, useEffect } from 'react'
-import Monday from './Monday/Monday.js'
+import mondaySdk from "monday-sdk-js";
+
+
 
 function App() {
 
-  const monday = new Monday(); // instanciar fora da função App pode evitar recriação
-  const [board, setBoard] = useState();
-
+  const monday = mondaySdk();
+  const [boards, setBoards] = useState([]);
+  
+  const create = (e) => {
+    e.preventDefault();
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
+    monday.api(`
+      mutation CreateNewItem{
+        create_item (board_id: 7564143508, item_name: "New Item") {
+          id
+        }
+      }
+       `).then(res => {
+           alert(res.data.create_item.id)
+       });
+  }
   useEffect(() => {
-    setBoard(monday.excecute());
-    console.log(board)
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
+    monday.api(`
+      query GetBoardItems{  
+       boards {  
+           items_page {  
+           items {  
+               id  
+               name  
+               column_values {  
+               id  
+               value  
+               }  
+           }  
+           }  
+       }  
+       }
+       `).then(res => {
+           //console.log(res)
+           setBoards(res.data.boards);
+       });
   }, []); 
 
   return (
     <>
-      <pre>{JSON.stringify(monday.excecute(), null, 2)}</pre>
+      {/*<pre>{JSON.stringify(boards, undefined, 2)}</pre>*/}
+      {
+        boards.map((board) => (
+          board.items_page.items.map((item) => (
+            <pre>{JSON.stringify(item, undefined, 2)}</pre>
+          ))
+        ))
+      }
+      <button onClick={(e) => create(e)}>Criar algo</button>
     </>
   )
 }
--- src/Monday/Monday.js
+++ src/Monday/Monday.js
@@ -2,7 +2,8 @@ import mondaySdk from "monday-sdk-js";
 
 export default class Monday{
     monday = mondaySdk();
-    async excecute() {
+
+    read() {
         let result
         this.monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
         this.monday.api(`
@@ -21,11 +22,33 @@ export default class Monday{
             }  
             }
             `).then(res => {
-                console.log(res)
-                result = res;
+                //console.log(res)
+                return res.data.create_item.id;
+            }
+        );
+    }
+
+    create() {
+        this.monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
+        this.monday.api(`
+           query GetBoardItems{  
+            boards {  
+                items_page {  
+                items {  
+                    id  
+                    name  
+                    column_values {  
+                    id  
+                    value  
+                    }  
+                }  
+                }  
+            }  
+            }
+            `).then(res => {
+                return res;
             }
             
         );
-        return result;
-        }
+    }
 }
