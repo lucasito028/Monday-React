dd7f042b3a1f26ec364a7cea1df659c9cb6ad7d2
lucasito028 <lucastoshihiroito28@gmail.com>
2024-10-14 11:00:14 -0300

    Melhoras

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import mondaySdk from "monday-sdk-js";
 function App() {
 
   const monday = mondaySdk();
-  const [boards, setBoards] = useState([]);
+  const [result, setResult] = useState([]);
   
   const create = (e) => {
     e.preventDefault();
@@ -19,6 +19,25 @@ function App() {
            alert(res.data.create_item.id)
        });
   }
+
+  const update = (e) => {
+    e.preventDefault();
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
+    monday.api(`
+      mutation UpdateColumnValue{
+      change_simple_column_value 
+      (board_id: 7564143508, 
+        item_id: 7564143554,
+        column_id:"name"
+        value: "Exemplo") {
+        id
+      }
+    }
+       `).then(res => {
+           alert(res.data.create_item.id)
+       });
+  }
+
   useEffect(() => {
     monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQxOTI5NDgxMCwiYWFpIjoxMSwidWlkIjo2NjkzNjYyNywiaWFkIjoiMjAyNC0xMC0wM1QyMzoxMzo1Ny4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjU4MDMzNTQsInJnbiI6InVzZTEifQ.eCp29EaFu911QT1avoSU2Kt1P2ht7U9ON_R1oaLes7I');
     monday.api(`
@@ -43,8 +62,7 @@ function App() {
         }  
       }
        `).then(res => {
-           //console.log(res)
-           setBoards(res.data.boards);
+           setResult(res.data.boards);
        });
   }, []); 
 
@@ -69,26 +87,56 @@ function App() {
 
   return (
     <>
-      {
-        boards.map((board) => {
-          const groupedItems = groupItemsByTitle(board.items_page.items);
-          return(
-            <div key={board.id}>
-              <h1>{board.name}</h1>
-              {Object.keys(groupedItems).map((groupKey) => (
-                <div>
-                  <h2>{groupedItems[groupKey].id}</h2>
-                  <h2>{groupedItems[groupKey].title}</h2>
-                  <pre>{JSON.stringify(groupedItems[groupKey], undefined, 2)}</pre>
-                </div>
-              ))}
-            </div>
-            )
-        }
-      )
-        
-      }
       <button onClick={(e) => create(e)}>Criar algo</button>
+      <button onClick={(e) => update(e)}>Alterar algo</button>
+
+      {result.map((board) => {
+        const groupedItems = groupItemsByTitle(board.items_page.items);
+        return (
+          <div key={board.id}>
+            <h1>Borda: {board.name}</h1>
+            {Object.keys(groupedItems).map((groupKey) => (
+              <div key={groupKey}>
+                <h2>Tabela: {groupedItems[groupKey].title}</h2>
+                <table>
+                  <thead>
+                    <tr>
+                      <td>Id</td>
+                      <td>Name</td>
+                      {groupedItems[groupKey].items.slice(0, 1).map((item) => (
+                      <>
+                        {item.column_values.map((column, index) => (
+                          <td key={index}>{column.id}</td>  
+                        ))}
+                      </>
+                    ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Id</td>
+                      <td>Name</td>
+                      {groupedItems[groupKey].items.map((item) => (
+                          <>
+                            {item.column_values.map((column, index) => (
+                              <td key={index}>{column.value}</td>  
+                            ))}
+                          </>
+                        ))}
+                    </tr>
+                  </tbody>
+                </table>
+
+                {groupedItems[groupKey].items.map((item) => (
+                  <pre key={item.id}>{JSON.stringify(item, undefined, 2)}</pre>
+                ))}
+              </div>
+            ))}
+          </div>
+        );
+      })}
+
+      {/*<pre>{JSON.stringify(result, undefined, 2)}</pre>*/}
     </>
   )
 }
