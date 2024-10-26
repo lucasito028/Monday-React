32cf9cf1e13277dbbc22a07140dce8aabff4c6e6
lucasito028 <lucastoshihiroito28@gmail.com>
2024-10-25 22:01:48 -0300

    Melhorias

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
     monday.api(`
       mutation CreateNewItem{
-        create_item (board_id: 7703061698, item_name: "Cenoura") {
+        create_item (board_id: 7709437460, item_name: "Cenoura") {
           id
         }
       }
@@ -27,10 +27,10 @@ function App() {
     monday.api(`
       mutation UpdateColumnValue{
       change_simple_column_value 
-      (board_id: 7703061698, 
-        item_id: 7627339961,
+      (board_id: 7709437460, 
+        item_id: 7709437471,
         column_id:"name"
-        value: "Carne") {
+        value: "Cenoura") {
         id
       }
     }
@@ -44,7 +44,7 @@ function App() {
     monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
     monday.api(`
       mutation CreateNewItem{
-        create_group(board_id: 7705275750,
+        create_group(board_id: 7705307859,
           group_name: "Binaldo"){
           id
         }
@@ -54,12 +54,54 @@ function App() {
        });
   }
 
+  const deleteGroup = (e) => {
+    e.preventDefault();
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
+    monday.api(`
+      mutation deleteGroup{
+        delete_group(board_id: 7705307859, group_id:"topics"){
+          id
+        }
+      }
+       `).then(res => {
+           alert(res.data.deleteGroup.id)
+       });
+  }
+
+  const updateGroup = (e) => {
+    e.preventDefault();
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
+    monday.api(`
+      mutation updateGroup{
+        update_group(board_id: 7705307859, group_id:"topics", group_attribute: title, new_value: "Cenoura"){
+          id
+        }
+      }
+       `).then(res => {
+           alert(res.data.update_group.id)
+       });
+  }
+
+  const updateBoard = (e) => {
+    e.preventDefault();
+    monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
+    monday.api(`
+      mutation updateBoard{
+        update_board(board_id: 7709354958, board_attribute:name, new_value: "Exemplo"){
+          id
+        }
+      }
+       `).then(res => {
+           alert(res.data.update_board.id)
+       });
+  }
+
   const deleteBoard = (e) => {
     e.preventDefault();
     monday.setToken('eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQyNzY5MDY4NiwiYWFpIjoxMSwidWlkIjo2Nzc0NTMzMywiaWFkIjoiMjAyNC0xMC0yNFQxMTo0NzozMy4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjYxNDkyNTIsInJnbiI6InVzZTEifQ.zhgRaEbrG4Pkbw0Uap6KX8GQM2DAHGaOTAjKeZfTBCA');
     monday.api(`
       mutation deleteBoard{
-        delete_board(board_id: 7705263259){
+        delete_board(board_id: 7709345811){
         id}
       }
        `).then(res => {
@@ -112,7 +154,9 @@ function App() {
       }
        `).then(res => {
            setResult(res.data.boards);
-       });
+       }).catch((err) => {
+        console.log(err)}
+       );
   }, []); 
 
   const groupItemsByTitle = (items) => {
@@ -138,9 +182,14 @@ function App() {
     <main>
       <button onClick={(e) => create(e)}>Criar algo</button>
       <button onClick={(e) => update(e)}>Alterar algo</button>
+
       <button onClick={(e) => createGroup(e)}>Criar Grupo</button>
+      <button onClick={(e) => deleteGroup(e)}>Deleta Grupo</button>
+      <button onClick={(e) => updateGroup(e)}>Alterar Grupo</button>
+
       <button onClick={(e) => deleteBoard(e)}>Deletar Board</button>
-      <button onClick={(e) => createBoard(e)}>Criar Borda</button>
+      <button onClick={(e) => updateBoard(e)}>Alterar Board</button>
+      <button onClick={(e) => createBoard(e)}>Criar Board</button>
 
       {result.map((board) => {
         const groupedItems = groupItemsByTitle(board.items_page.items);
